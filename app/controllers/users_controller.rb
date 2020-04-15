dbcde6781cd0c4a359c636237989d00e22089cb2
kazu-daimar <daima.exe@gmail.com>
2020-04-15 14:52:37 +0900

    implementation_end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.search(params[:keyword], current_user.id)
+    respond_to do |format|
+      format.json
+    end
+  end
+
   def edit
   end
 
