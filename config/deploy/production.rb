94547f83d1b30877296c9b84bffe1d961c0186af
kazu-daimar <daima.exe@gmail.com>
2020-04-17 03:35:13 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-servcer '52.198.36.163', user: 'ec2-user', roles: %w{app db web}
+server '52.198.36.163', user: 'ec2-user', roles: %w{app db web}
